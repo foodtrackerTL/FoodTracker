a24ef8be02bac63e9a9c7d1a259dd7c670c690cb
foodtrackerTL <65966420+foodtrackerTL@users.noreply.github.com>
2020-05-28 20:09:50 +0300

    Add files via upload

---
+++ projectTL/AdminTrue.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+  <link rel="stylesheet" type="text/css" href="main.css">
+
+	<?php session_start();
+	include 'index.html';
+		if($_SESSION['AdminFlag']==TRUE){  ?>	
+			 <script type="text/javascript">
+        document.getElementById("9").style.display = "block";
+        document.getElementById("10").style.display = "block";
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "none";
+    document.getElementById("3").style.display = "block";
+     document.getElementById("4").style.display = "block";
+
+   
+  </script>
+
+  
+<div class="outer-container">lalalalalalala
+  <div class="content">
+lalalalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+	<?php	}  ?>
+
+</body>
+</html>
